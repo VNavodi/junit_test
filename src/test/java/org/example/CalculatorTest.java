fef213b882d36d663fa3be7ffa5319e607e7a898
VNavodi <videeshanavodi1234@gmail.com>
2026-05-08 12:08:32 +0530

    CI

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    void testAddition() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(2, calc.divide(10, 5));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        assertThrows(ArithmeticException.class, () -> calc.divide(10, 0));
+    }
+}
